c1bb2c18c88beca78cef33f71d48122acd1e0475
Benjamin Rittenhouse <ben@Bens-MacBook-Air.local>
2021-12-18 15:48:07 -0500

    updated structure, working on flask

--- viz.py
+++ viz.py
@@ -5,31 +5,42 @@
 '''
 
 import spacy
+from flask import *
+import requests
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def main():
+	if request.method == 'POST':
+		sentence_inp = request.form.get('user_sentence')
+		if sentence_inp != '':
+			sentence = request.form['user_sentence']
+			pieces = [] # pieces of sentence init
+			pieces = parse_sentence(sentence)
+			return render_template('index_updated.html', subject = pieces[0], object = pieces[1], verb = pieces[2])
+	else:
+		return render_template('index.html')
 
 nlp = spacy.load("en_core_web_lg")
 
 # MAIN
-inp = ''
 
-while inp != "exit":
-	inp = input("Enter a sentence:\n")
 	#words = inp.split(' ')
-	
-	doc=nlp(inp)
+def parse_sentence(s):
+
+	doc=nlp(s)
 
 	subj = [tok for tok in doc if (tok.dep_ == "nsubj") ]
 	obj = [tok for tok in doc if (tok.dep_ == "dobj") ]
 	verb = [tok for tok in doc if (tok.dep_ == "ROOT") ]
-	
 
-	'''
-	print("Subject: ")
-	print(subj)
-	print("Object: ")
-	print(obj)
-	print("Verb: ")
-	print(verb)
-	'''
+	ret = []
+	ret.append(subj)
+	ret.append(obj)
+	ret.append(verb)
+
+	return ret
 
 
 
